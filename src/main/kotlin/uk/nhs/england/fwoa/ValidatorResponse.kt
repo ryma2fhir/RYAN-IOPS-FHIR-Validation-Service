335a5be0a99769894038acbec98271820819f240
Kevin Mayfield <KevinMayfield@users.noreply.github.com>
2022-12-19 09:40:41 +0000

    Removed lombok

--- src/main/kotlin/uk/nhs/england/fwoa/ValidatorResponse.kt
+++ src/main/kotlin/uk/nhs/england/fwoa/ValidatorResponse.kt
@@ -1,18 +1,12 @@
 package uk.nhs.england.fwoa
 
-import lombok.Builder
-import lombok.Value
+data class ValidatorResponse (
+    var isSuccessful: Boolean = false,
+    var errorMessages: List<ValidatorErrorMessage>? = null
+)
 
-@Builder
-@Value
-class ValidatorResponse {
-    private val isSuccessful = false
-    private val errorMessages: List<ValidatorErrorMessage>? = null
-}
 
-@Builder
-@Value
-internal class ValidatorErrorMessage {
+class ValidatorErrorMessage {
     private val severity: String? = null
     private val msg: String? = null
 }
