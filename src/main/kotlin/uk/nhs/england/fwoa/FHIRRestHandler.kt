4e9b49384aed00ccc3a2ac63560ea083d5641d4c
Kevin Mayfield <KevinMayfield@users.noreply.github.com>
2022-12-22 12:00:39 +0000

    AWS Spring Lambda support

--- src/main/kotlin/uk/nhs/england/fwoa/FHIRRestHandler.kt
+++ src/main/kotlin/uk/nhs/england/fwoa/FHIRRestHandler.kt
@@ -1,12 +1,12 @@
 package uk.nhs.england.fwoa
 
 import com.amazonaws.serverless.exceptions.ContainerInitializationException
+import com.amazonaws.serverless.proxy.internal.LambdaContainerHandler
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler
-
 import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
@@ -30,6 +30,20 @@ class FHIRRestHandler : RequestStreamHandler {
 
     @Throws(IOException::class)
     override fun handleRequest(inputStream: InputStream?, outputStream: OutputStream?, context: Context?) {
+       /* handler.stripBasePath("/validation/FHIR/R4")
+        val request = LambdaContainerHandler.getObjectMapper().readValue(inputStream, AwsProxyRequest::class.java)
+        if (context != null) {
+            context.logger.log("Path before: " + request.path)
+        };
+        if (inputStream != null) {
+            inputStream.reset()
+        }
+
+        */
         handler.proxyStream(inputStream, outputStream, context)
+        /*
+        if (context != null) {
+            context.logger.log("Path after: " + request.path)
+        };*/
     }
 }
